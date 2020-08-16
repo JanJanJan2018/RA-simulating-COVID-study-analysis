37c5600974ce8c13649e2eba916323a6b0943b99
JanJanJan2018 <39683658+JanJanJan2018@users.noreply.github.com>
2020-08-15 18:24:33 -0700

    Add files via upload

--- geneCards2.R
+++ geneCards2.R
@@ -39,7 +39,9 @@ find25genes <- function(protein){
   url <- as.character(url)
   url <- gsub('protein',protein, url)
   
-  webpage <- read_html(url,encoding = "UTF-8")
+#  webpage <- read_html(url,options=pedantic,encoding = "UTF-8")
+  
+webpage <- read_html(url,encoding = "UTF-8")
   
   protein_html <- html_nodes(webpage,'.symbol-col a')
   protein1 <- html_text(protein_html)
@@ -109,6 +111,9 @@ getSummaries <- function(gene,protein){
   url <- as.character(url)
   url <- gsub('GENE',gene,url)
   url <- gsub('protein',protein, url)
+
+  #webpage <- read_html(url,options=pedantic,encoding = "UTF-8")
+  
   
   webpage <- read_html(url,encoding = "UTF-8")
   
@@ -187,7 +192,8 @@ getGeneSummaries <- function(protein){
                      sep=',', header=T, na.strings=c('',' ','NA'), stringsAsFactors = F)
   names <- header$x
   colnames(table) <- names
-  
+  table$EnsemblID <- trimws(as.character(paste(table$EnsemblID)), 
+                            which='right',  whitespace='\n')
   fileName <- paste('proteinGeneSummaries_',protein,'.csv',sep='')
   write.csv(table, fileName, row.names=FALSE)
   return(list(table,fileName))
@@ -195,4 +201,123 @@ getGeneSummaries <- function(protein){
 
 
 #getGeneSummaries('estrogen')
+
+#added the Ensembl ID to the data frame in the getSummaries() to be getSummaries2()
+
+#get the Ensembl ID.
+
+getSummaries2 <- function(gene,protein){
+  url <- 'https://www.genecards.org/cgi-bin/carddisp.pl?gene=GENE&keywords=protein'
+  
+  protein <- as.character(protein)
+  protein <- tolower(protein)
+  protein <- gsub(' ',',',protein)
+  gene <- as.character(gene)
+  gene <- tolower(gene)
+  
+  url <- as.character(url)
+  url <- gsub('GENE',gene,url)
+  url <- gsub('protein',protein, url)
+  
+# webpage <- read_html(url,options=pedantic,encoding = "UTF-8")
+  webpage <- read_html(url,encoding = "UTF-8")
+  
+  Entrez_html <- html_nodes(webpage, '.gc-section-header+ .gc-subsection p')
+  Entrez <- html_text(Entrez_html) 
+  
+  GeneCards_html <- html_nodes(webpage, '.gc-subsection-header+ p')
+  GeneCards <- html_text(GeneCards_html) 
+  
+  UniProt_html <- html_nodes(webpage, '#summaries li:nth-child(1) div')
+  UniProtKB <- html_text(UniProt_html) 
+  
+  ensemblhtml <- html_nodes(webpage,'#aliases_descriptions .gc-subsection:nth-child(2) .gc-subsection-inner-wrap')
+  ensemblID <- html_text(ensemblhtml)
+  
+  ensemble0 <- ifelse(length(ensemblID)==0,'no Ensembl_ID',
+                      as.character(paste(ensemblID)))
+  ensemble1 <- as.data.frame(ensemble0)
+  colnames(ensemble1) <- 'EnsemblID'
+
+  
+  ##########
+  ensemble1$EnsemblID <- trimws(ensemble1$EnsemblID, 
+                                which='left',whitespace=' ')
+  ensemble1$EnsemblID <- trimws(ensemble1$EnsemblID, 
+                                which='right',whitespace=' ')
+  
+  split1 <- strsplit(ensemble1$EnsemblID,split='\\n',perl=T)
+  df <- as.data.frame(split1)
+  colnames(df) <- c('EnsemblID')
+  df_ensemble <- df[4,]
+  df_ensemble <- as.data.frame(df_ensemble)
+  colnames(df_ensemble) <- 'EnsemblID'
+  row.names(df_ensemble) <- NULL
+  df_ensemble$EnsemblID <- trimws(df_ensemble$EnsemblID,which='left',
+                                  whitespace=' ')
+  df_ensemble$EnsemblID <- gsub('Ensemble: ','',df_ensemble$EnsemblID)
+  ensemble1 <- df_ensemble
+  ensemble0 <- ifelse(length(ensemble1$EnsemblID)==0,'no Ensembl_ID',
+                      as.character(paste(ensemble1$EnsemblID)))
+  ensemble1 <- as.data.frame(ensemble0)
+  colnames(ensemble1) <- 'EnsemblID'
+  ##########
+  
+  Entrez0 <- ifelse(length(Entrez)==0, 'no summary',as.character(paste(Entrez)))
+  Entrez1 <- as.data.frame(Entrez0)
+  colnames(Entrez1) <- 'EntrezSummary'
+  
+  GeneCards0 <- ifelse(length(GeneCards)==0,'no summary',
+                       as.character(paste(GeneCards)))
+  GeneCards1 <- as.data.frame(GeneCards0)
+  colnames(GeneCards1) <- 'GeneCardsSummary'
+  
+  UniProtKB0 <- ifelse(length(UniProtKB)==0,'no summary',
+                       as.character(paste(UniProtKB)))
+  UniProtKB1 <- as.data.frame(UniProtKB0)
+  colnames(UniProtKB1) <- 'UniProtKB_Summary'
+  
+  ensemble1$EnsemblID <- as.character(paste(ensemble1$EnsemblID))
+  ensemble1$EnsemblID <- gsub('Ensembl: ','',
+                              ensemble1$EnsemblID)
+  ensemble1$EnsemblID <- trimws(as.character(paste(ensemble1$EnsemblID)),
+                                which='right',
+                                whitespace='\n')
+  Entrez1$EntrezSummary <- as.character(paste(Entrez1$EntrezSummary))
+  Entrez1$EntrezSummary <- gsub('\n','',Entrez1$EntrezSummary)
+  
+  GeneCards1$GeneCardsSummary <- as.character(paste(GeneCards1$GeneCardsSummary))
+  GeneCards1$GeneCardsSummary <- gsub('\n','',GeneCards1$GeneCardsSummary)
+  
+  UniProtKB1$UniProtKB_Summary <- as.character(paste(UniProtKB1$UniProtKB_Summary))
+  UniProtKB1$UniProtKB_Summary <- gsub('\n','',UniProtKB1$UniProtKB_Summary)
+  
+  date <- as.data.frame(rep(date(),length(Entrez1$EntrezSummary)))
+  colnames(date) <- 'todaysDate'
+  
+  protein2 <- gsub(',','-',protein)
+  
+  proteinName <- as.data.frame(rep(protein2,length(Entrez1$EntrezSummary)))
+  colnames(proteinName) <- 'proteinSearched'
+  
+  gene <- as.data.frame(rep(toupper(gene),length(Entrez1$EntrezSummary)))
+  colnames(gene) <- 'gene'
+  
+  tableProtein <- cbind(proteinName,gene,ensemble1,Entrez1,GeneCards1,UniProtKB1,date)
+  
+  setwd(Gene_Path)
+  
+  
+  write.table(tableProtein, 
+              paste(protein2,"summary.csv",sep=''), append=TRUE,
+              col.names=FALSE, sep=",", quote=TRUE,qmethod="double",
+              row.names=FALSE)
+  names <- colnames(tableProtein)
+  write.csv(names,paste('geneHeader_summary_',protein2,'.csv',sep=''),row.names=FALSE)
+  
+  setwd('../')
+  return(gene)
+}
+
+
 
